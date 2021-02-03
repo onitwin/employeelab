c7c7fb547a9aac4412a926d85cfc53a272113a5e
Jack Portwood <Jack.Portwood14@gmail.com>
2021-02-03 10:36:53 +0000

    Employee class started

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -1,4 +1,38 @@
 package staff;
 
 public abstract class Employee {
+
+    private String name;
+    private String ni;
+    private double salary;
+
+    public Employee(String name, String ni, double salary){
+        this.name = name;
+        this.ni = ni;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNi() {
+        return ni;
+    }
+
+    public void setNi(String ni) {
+        this.ni = ni;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
